43412d78a391b8addfb800237b549a4dd59c8fc8
timothyoesch <54284649+timothyoesch@users.noreply.github.com>
2022-03-25 07:53:18 +0100

    Tailwind init | Devbutton

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./App.css";
+
+const App = (props) => <div></div>;
+
+App.propTypes = {
+  // bla: PropTypes.string,
+};
+
+App.defaultProps = {
+  // bla: 'test',
+};
+
+export default App;
