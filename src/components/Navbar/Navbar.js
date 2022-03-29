4e60216428a95c9ccbc13cf560d4a87eeda42cd6
timothyoesch <54284649+timothyoesch@users.noreply.github.com>
2022-03-29 17:17:13 +0200

    Scroll Navbar DESK

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,35 +4,74 @@ import "./Navbar.css";
 import Logo from "./logo";
 import Button from "../Button/Button";
 
-const Navbar = (props) => (
-  <div className="NavbarWrapper">
-    <nav className="flex justify-between mx-auto my-3">
-      <div id="logo-container">
-        <Logo />
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isScroll: "",
+    };
+  }
+
+  componentDidMount() {
+    if (window.scrollY > 0) {
+      this.setState({
+        isScroll: true,
+      });
+    }
+
+    document.addEventListener("scroll", () => {
+      if (window.scrollY > 0) {
+        this.setState({
+          isScroll: true,
+        });
+      } else {
+        this.setState({
+          isScroll: false,
+        });
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="NavbarWrapper">
+        <div
+          className={
+            this.state.isScroll
+              ? "scrollbar scroll flex w-full"
+              : "scrollbar flex w-full"
+          }
+        >
+          <nav className="flex justify-between mx-auto my-3">
+            <div id="logo-container" className="my-auto">
+              <Logo />
+            </div>
+            <div id="menu-container" className="my-auto">
+              <ul className="flex">
+                <li>
+                  <a href="#">Über mich</a>
+                </li>
+                <li>
+                  <a href="#">Anliegen</a>
+                </li>
+                <li>
+                  <Button link="#mitmachen" className="neg line">
+                    Mitmachen
+                  </Button>
+                </li>
+                <li>
+                  <Button link="#spenden" className="neg line marginleft">
+                    Spenden
+                  </Button>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
       </div>
-      <div id="menu-container" className="my-auto">
-        <ul className="flex">
-          <li>
-            <a href="#">Über mich</a>
-          </li>
-          <li>
-            <a href="#">Anliegen</a>
-          </li>
-          <li>
-            <Button link="#mitmachen" className="neg line">
-              Mitmachen
-            </Button>
-          </li>
-          <li>
-            <Button link="#spenden" className="neg line marginleft">
-              Spenden
-            </Button>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  </div>
-);
+    );
+  }
+}
 
 Navbar.propTypes = {
   // bla: PropTypes.string,
