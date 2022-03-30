6138a1b327e4c759b53ab942583b77796c2159dc
timothyoesch <54284649+timothyoesch@users.noreply.github.com>
2022-03-31 01:17:00 +0200

    Shizzle

--- wordpress/wp-content/themes/rem-publicam-capessere/footer.php
+++ wordpress/wp-content/themes/rem-publicam-capessere/footer.php
@@ -7,7 +7,6 @@ get_template_part("/templates/partials/footer");
 ?>
 
 </body>
-<script src="http://localhost:35729/livereload.js"></script>
 <?php
 wp_footer();
 ?>
--- wordpress/wp-content/themes/rem-publicam-capessere/functions.php
+++ wordpress/wp-content/themes/rem-publicam-capessere/functions.php
@@ -9,3 +9,8 @@ function rpc_scripts() {
     wp_enqueue_script( 'bundle', get_template_directory_uri() . '/dist/app.js', array(), "1.0.0", true );
 }
 add_action( 'wp_enqueue_scripts', 'rpc_scripts' );
+
+function rpc_theme_support() {
+    add_theme_support( 'title-tag' );
+}
+add_action( 'after_setup_theme', 'rpc_theme_support' );
