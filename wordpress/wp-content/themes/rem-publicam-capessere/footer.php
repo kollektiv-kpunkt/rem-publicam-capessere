4d917c834dc57bcb7d04360e448ab6aa7293a063
Timothy Justin Oesch <54284649+timothyoesch@users.noreply.github.com>
2022-03-30 12:56:22 +0200

    WordPress & Webpack init

---
+++ wordpress/wp-content/themes/rem-publicam-capessere/footer.php
@@ -0,0 +1,6 @@
+</div>
+</body>
+<?php
+wp_footer();
+?>
+</html>
---
+++ wordpress/wp-content/themes/rem-publicam-capessere/functions.php
@@ -0,0 +1,9 @@
+<?php
+/* RemPublicamCapessere */
+
+function rpc_scripts() {
+    wp_enqueue_style( 'tailwind-bundle', "https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" );
+    wp_enqueue_style( 'bundle', get_template_directory_uri() . '/dist/style.bundle.css' );
+    wp_enqueue_script( 'bundle', get_template_directory_uri() . '/dist/app.js', array(), '1.0.0', true );
+}
+add_action( 'wp_enqueue_scripts', 'rpc_scripts' );
