ba9d55cb87efd148eeae259384c1d2f09047a492
Timothy Justin Oesch <54284649+timothyoesch@users.noreply.github.com>
2022-03-30 17:00:33 +0200

    Update functions.php

--- wordpress/wp-content/themes/rem-publicam-capessere/functions.php
+++ wordpress/wp-content/themes/rem-publicam-capessere/functions.php
@@ -2,7 +2,6 @@
 /* RemPublicamCapessere */
 
 function rpc_scripts() {
-    // wp_enqueue_style( 'tailwind-bundle', "https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" );
     wp_enqueue_style( 'theme', get_template_directory_uri() . '/dist/theme.css' );
     wp_enqueue_style( 'bundle', get_template_directory_uri() . '/dist/style.css' );
     wp_enqueue_script( 'bundle', get_template_directory_uri() . '/dist/app.js', array(), '1.0.0', true );
